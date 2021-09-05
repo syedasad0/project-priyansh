2bf52d6352bbb0002d0ad4a58d75b68f0c2fff89
Syed Mohammad Asad <syedasad0@gmail.com>
2021-09-05 18:25:43 +0530

    Addition and Substraction added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+//Git is a Version Control System
 console.log("Hello Priyansh!!!!");
 console.log("Hello From JS!!!!");
+const a =10;
+const b = 20;
+const sum, product;
+sum = a+b;
+product = a*b;
+console.log('Sum = ', sum);
+console.log('Product = ', product);
